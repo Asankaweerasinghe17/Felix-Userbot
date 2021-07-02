3082502e0ced5253d5fae4cc5405fbbc745996f0
TOXIC DEVIL <jinnherefan2021@gmail.com>
2021-07-02 21:12:36 +0530

    Felix Userbot ✅️

--- plugins/_felix.js
+++ plugins/_felix.js
@@ -1,11 +1,3 @@
-/* Copyright (C) 2020 Yusuf Usta.
-
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
-*/
-
 const TOXIC_DEVIL = require('../events');
 const Config = require('../config');
 const {MessageType} = require('@adiwajshing/baileys');
